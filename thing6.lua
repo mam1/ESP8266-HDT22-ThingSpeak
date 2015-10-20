6a71cf093a7222d1b3cd33e6377f9153e567b830
mam1 <mam1@cornell.edu>
2015-10-19 22:48:54 -0400

    new code to read sensors for all bays

--- thing6.lua
+++ thing6.lua
@@ -1,54 +1,13 @@
-version = "0.5"
-delay = 60000
-done = true
-
--- send readings to ThingSpeak chanel
-function postThingSpeak(f1,v1,f2,v2,f3,v3,f4,v4)
-    connout = nil
-    connout = net.createConnection(net.TCP, 0)
-    connout:on("receive", function(connout, payloadout)
-        if (string.find(payloadout, "Status: 200 OK") ~= nil) then
-            print("Posted OK");
-        end
-    end)
-    connout:on("connection", function(connout, payloadout) 
-        print ("Posting...");       
-        connout:send("GET /update?api_key=1Y898KJ1K8YQJTU4&field"
-        .. f1
-        .."=" 
-        .. v1
-        .. "&field"
-        .. f2
-        .. "="
-        .. v2
-        "&field"
-        .. f2
-        .. "="
-        .. v2
-        "&field"
-        .. f2
-        .. "="
-        .. v2
-        .. " HTTP/1.1\r\n"
-        .. "Host: api.thingspeak.com\r\n"
-        .. "Connection: close\r\n"
-        .. "Accept: */*\r\n"
-        .. "User-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n"
-        .. "\r\n")
-    end)
-    connout:on("disconnection", function(connout, payloadout)
-        connout:close();
-        collectgarbage(); 
-    end)
-    connout:connect(80,'api.thingspeak.com')
-end
+version = "0.6"
+KEY = "1Y898KJ1K8YQJTU4"
+PINS = {2,4}  -- HDT22 sensor's data pins
+delay = 10000
  
--- read sensor
+-- *************** read sensor return temp and humidity converted to text ****************
 function readHDT22(pin)
     dht22 = require("dht22_min")
     local t,h,tt,hh
     print("\ngeting HDT22 data from pin "..pin)
-    --read sensor
     dht22.read(PIN)
     t = dht22.getTemperature()
     h = dht22.getHumidity()
@@ -62,33 +21,72 @@ function readHDT22(pin)
   --release DHT22 module
     dht22 = nil
     package.loaded["dht22"]=nil  
-  --return temperature and humidy text strings
+  --return temperature and humidly text strings
   return tt, hh
 end
 
--- read sensors
+-- *************** send readings to ThingSpeak channel ****************************
+function postThingSpeak(v1,v2,v3,v4,v5,v6)
+    connout = nil
+    connout = net.createConnection(net.TCP, 0)
+    connout:on("receive", function(connout, payloadout)
+        if (string.find(payloadout, "Status: 200 OK") ~= nil) then
+            print("Posted OK");
+        end
+    end)
+    connout:on("connection", function(connout, payloadout) 
+        print ("Posting...");       
+        connout:send("GET /update?api_key="
+        .. KEY
+        .. "1Y898KJ1K8YQJTU4&field1="
+        .. v1
+        .. "&field2="
+        .. v2
+        .. "&field3="
+        .. v3
+        .. "&field4="
+        .. v4
+        .. "&field5="
+        .. v5
+        .. "&field6="
+        .. v6
+        .. " HTTP/1.1\r\n"
+        .. "Host: api.thingspeak.com\r\n"
+        .. "Connection: close\r\n"
+        .. "Accept: */*\r\n"
+        .. "User-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n"
+        .. "\r\n")
+    end)
+    connout:on("disconnection", function(connout, payloadout)
+        connout:close();
+        collectgarbage(); 
+    end)
+    connout:connect(80,'api.thingspeak.com')
+end
+
+-- *************** read sensors ************************************************
 function update()
     dht22 = require("dht22_min")
-    local pins,fields
-    local tt, hh
-
+    local pins,i
+    local tt, hh, 
   --read a list of sensor pins
-    pins = {1, 2, 4}
-    fields = {
-    
-    iter = list_iter(pins)    -- creates the iterator
+    i = 1
+    iter = list_iter(PINS)    -- creates the iterator
     while true do
-      local pin = iter(pins)   -- calls the iterator
+      local pin = iter(PINS)   -- calls the iterator
       if pin == nil then break end
-        tt, hh = readHDT22(pin)
-      --display value strings on console
+        tt[i], hh[i] = readHDT22(pin)
         print("    pin "..pin.." - temperature "..tt .. "\n    humidity " ..hh) 
+        i = i + 1
+    end
+    tt[3] = 50
+    hh[3] = 60
   --send values to thingspeak
     print("    send data to thingspeak")
-    done = false
-    postThingSpeak(1,tt,3,hh)
-    print("done with update")    
+    postThingSpeak(tt[1],hh[1],tt[2],hh[2],tt[3],hh[3])
+    print("update complete")    
 end
+
 -- ***************************start main code **********************************
 print("\n\nHDT22 sensor / write to thingspeak  - version "..version.."\n") 
 print("running update every " .. delay .. "ms")
