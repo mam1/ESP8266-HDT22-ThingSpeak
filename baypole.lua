462f69fba21ebabaef5327434f6f3a17e0b7ef43
mam1 <mam1@icloud.com>
2015-10-23 12:25:52 -0400

    baypole looking good, ready to test on new hardware when it arrives

--- baypole.lua
+++ baypole.lua
@@ -3,39 +3,31 @@ CHANNEL_API_KEY = "1Y898KJ1K8YQJTU4"
 delay = 60000
 PINS =   {4,4,2,2,4,4}
 FIELDS = {1,2,3,4,5,6}
-er = false
 pinptr = 1
-
+--get data from DHT22 sensor on <pin>
 function rdDHT22(pin)
 	local tmp, hmd
-    print("  reading pin "..PINS[pinptr])
+    print("\n  reading pin "..PINS[pinptr])
     dht22 = require("dht22_min")
---read sensor
+	--read sensor
     dht22.read(PINS[pinptr])
     tmp = dht22.getTemperature()
     hmd = dht22.getHumidity()
 	if tmp == nil then
-    print("*** Error reading temperature from DHT22")
-       er = true
+    	print("*** Error reading temperature from DHT22")
     end
     if hmd == nil then
         print("*** Error reading humidity from DHT22")
-        er = true
     end
-    if er then
-        er = false
-        dht22 = nil
-        package.loaded["dht22"]=nil
-        rdDHT22(pin)
-    end
---release DHT22 module
+	--release DHT22 module
     dht22 = nil
     package.loaded["dht22"]=nil
 	return tmp, hmd 
 end
 
+--post data <value> to ThingSpeak api key <key>, field <field>
 function post(key,field,value)
-    print("  posting pin "..PINS[pinptr].." data to field "..field.." value is "..value)   
+    print("    posting pin "..PINS[pinptr].." data to field "..field.." value is "..value)   
     connout = nil
     connout = net.createConnection(net.TCP, 0)
     connout:on("receive", function(connout, payloadout)
@@ -43,7 +35,6 @@ function post(key,field,value)
             print("    Posted OK");
         end
     end)
- 
     connout:on("connection", function(connout, payloadout) 
         print ("    Posting...");       
         connout:send("GET /update?api_key="
@@ -59,20 +50,17 @@ function post(key,field,value)
         .. "User-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n"
         .. "\r\n")
     end)
- 
     connout:on("disconnection", function(connout, payloadout)
         connout:close();
         collectgarbage(); 
     end)
- 
     connout:connect(80,'api.thingspeak.com')
 end
 
 function update()
 	local t, h,send
-    print("updating ....")
+
     t, h = rdDHT22(PINS[pinptr])
-    print("    t "..t.."  h "..h)
     if pinptr % 2 ~= 0 then
         send = (t*9)/5 + 320
     	print("    posting temperature ")       
@@ -89,7 +77,8 @@ end
 if (#PINS ~= #FIELDS) then 
 	print("\n***** pin count and field count do not match\naborting")
 else
-	print("\n\n 6 HDT22 sensors / write to thingspeak  - version "..version.."\n")
-	print("running update every " .. delay .. "ms")
+    print("\n\n*** baypole.lua  version "..version.." ***")
+	print("  reading "..(#PINS / 2).." HDT22 sensors\n  posting data to ThingSpeak api key "..CHANNEL_API_KEY)
+	print("  running update every " .. delay .. "ms\n")
 	tmr.alarm(0, delay, 1, update) 
 end
